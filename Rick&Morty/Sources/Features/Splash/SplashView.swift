63bec310bd06538c375fbdb5aee63810dd449ba3
Eduardo Frederico <eduardo.frederico@icloud.com>
2026-03-04 22:29:45 -0300

    Preparando a SplashScreen.

---
+++ Rick&Morty/Sources/Features/Splash/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  Rick&Morty
+//
+//  Created by Maria Eliza on 04/03/26.
+//
+
+import Foundation
---
+++ Rick&Morty/Sources/Features/Splash/SplashViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewController.swift
+//  Rick&Morty
+//
+//  Created by Maria Eliza on 04/03/26.
+//
+
+import Foundation
