18a49272dd8e68ecf2be0b9d364e0145ca75132c
Eduardo Frederico <eduardo.frederico@icloud.com>
2026-03-30 23:59:31 -0300

    Criando System Design e fugindo de Magic Numbers.

---
+++ Rick&Morty/Sources/Features/Constants/Metrics/Metrics-constants.swift
@@ -0,0 +1,8 @@
+//
+//  Metrics-constants.swift
+//  Rick&Morty
+//
+//  Created by Maria Eliza on 30/03/26.
+//
+
+import Foundation
---
+++ Rick&Morty/Sources/Features/Constants/Text/Text-constants.swift
@@ -0,0 +1,8 @@
+//
+//  Text-constants.swift
+//  Rick&Morty
+//
+//  Created by Maria Eliza on 30/03/26.
+//
+
+import Foundation
