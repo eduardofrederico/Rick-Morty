2f3163076a5c4c4617e86af64fbb4ccad173639f
Eduardo Frederico <eduardo.frederico@icloud.com>
2026-08-19 23:39:27 -0300

    Factory Pattern e aplicação do App Icon.

---
+++ Rick&Morty/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactory.swift
+//  Rick&Morty
+//
+//  Created by Maria Eliza on 19/08/26.
+//
+
+import Foundation
---
+++ Rick&Morty/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactoryProtocol.swift
+//  Rick&Morty
+//
+//  Created by Maria Eliza on 19/08/26.
+//
+
+import Foundation
